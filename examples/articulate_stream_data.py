262396a4679e05ba281a88d2cae5073084d2ee1b
Rowan Ferrabee <rowanferrabee@gmail.com>
2019-02-14 01:19:53 -0500

    Complete bt protocol v1 including streaming, standby, ack, lra bluetooth

--- examples/articulate_stream_data.py
+++ examples/articulate_stream_data.py
@@ -8,20 +8,152 @@ import Adafruit_BluefruitLE
 from Adafruit_BluefruitLE.services import UART
 from Adafruit_BluefruitLE.services import DeviceInformation
 
-
 # Get the BLE provider for the current platform.
 ble = Adafruit_BluefruitLE.get_provider()
 
 import random
 import string
-
+import threading
+from msg_defs import *
 
 # Main function implements the program logic so it can run in a background
 # thread.  Most platforms require the main thread to handle GUI events and other
 # asyncronous events like BLE actions.  All of the threading logic is taken care
 # of automatically though and you just need to provide a main function that uses
 # the BLE provider.
+
+STARTING_STREAM = 0
+STREAMING = 1
+state = STARTING_STREAM
+
+fsmState = 0
+fsmCounter = 0
+
+imuDataAvailable = 0
+latestImuData = ''
+
+def imuServerWorker():
+    global imuDataAvailable
+    global latestImuData
+
+    time.sleep(1)
+    HOST = ''               # Symbolic name meaning all available interfaces
+    PORT = 5432             # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(2)
+    conn, addr = s.accept()
+    print('IMU server connected on: {}'.format(addr))
+
+    try:
+        while(True):
+            if (imuDataAvailable):
+                conn.send(latestImuData)
+                imuDataAvailable = False
+                print('Sent IMU Data')
+    finally:
+        if conn is not None:
+            print('Closing socket connection')
+            conn.close()
+        else:
+            print('No socket connection')
+
+
+def bluetoothWorker(articulate_board):
+
+    global imuDataAvailable
+    global latestImuData
+
+    MAX_NUM_ERRORS = 4
+
+    while(True):
+
+        msg = ''
+
+        num_errors = 0
+        while((len(msg) < PACKET_SIZE) and (num_errors < MAX_NUM_ERRORS)):
+            received = articulate_board.read(timeout_sec=1)
+            if received is not None:
+                msg = msg + (received)
+            else:
+                time.sleep(0.01)
+                num_errors += 1
+
+        if (len(msg) < PACKET_SIZE):
+            print("Failed to read packet. Num errors: {}".format(num_errors))
+            continue
+
+        if (msg[POS_SOP] != chr(SOP)):
+            print("Incorrect SOP")
+            continue
+
+        parsed_message = None
+        if (msg[POS_DATA] == chr(IMU_DATA_MSG)):
+            parsed_message = IMUDataMsg.fromBytes(msg)
+            if (imuDataAvailable == False):
+                latestImuData = msg[2:18]
+                imuDataAvailable = True
+
+        elif (msg[POS_DATA] == chr(ACK_MSG)):
+            parsed_message = ACKMsg.fromBytes(msg)
+
+        elif (msg[POS_DATA] == chr(STANDBY_MSG)):
+            parsed_message = StandbyMsg.fromBytes(msg)
+
+        else:
+            print("Invalid Data Type")
+            continue
+
+        print("Received: {}".format(parsed_message))
+
+
+def keepAliveWorker(articulate_board):
+    # Send periodic messages to continue streaming 
+    test_standby = True
+
+    i = 0
+    while(True):
+        if (test_standby):
+            if (i%120 < 6):
+                print("Sending standby message")
+                articulate_board.write(StandbyMsg.toBytes())
+            else:
+                print("Sending stream message")
+                msg = StreamMsg(100)    # Set IMU streaming period (in ms)
+                articulate_board.write(msg.toBytes())
+
+        else:
+            print("Sending stream message")
+            msg = StreamMsg(50)    # Set IMU streaming period (in ms)
+            articulate_board.write(msg.toBytes())
+
+        time.sleep(1)
+        i=i+1
+
+def lraCmdWorker(articulate_board):
+
+    onIntensities = [int(127)]*8
+    onMsgBytes = LRACmdMsg(onIntensities).toBytes()
+
+    offIntensities = [int(0)]*8
+    offMsgBytes = LRACmdMsg(offIntensities).toBytes()
+
+    i = 0
+    while(True):
+        if (i%2 == 0):
+            print("Sending LRA on message")
+            articulate_board.write(onMsgBytes)
+            time.sleep(1)
+        else:
+            print("Sending LRA off message")
+            articulate_board.write(offMsgBytes)
+            time.sleep(3)
+
+        i += 1
+
 def main():
+
     target_device_name = u'RN4871-1444'
     target_device = None
 
@@ -78,7 +210,6 @@ def main():
 
     # Once connected do everything else in a try/finally to make sure the device
     # is disconnected when done.
-    # try:
     print('Discovering services...')
     UART.discover(target_device)
 
@@ -87,78 +218,28 @@ def main():
 
     time.sleep(1.0)
 
-    print('Sending stream command until ok is received')
+    try:
+        print("Starting bt thread")
+        btThread = threading.Thread(target=bluetoothWorker, args=(articulate_board,))
+        btThread.start()
 
-    stream_cmd =  'STREAMPLSSTREAMPLSSTREAMPLSSTREAMPLSSS'
-    cmd_checksum = chr(sum([ord(x) for x in stream_cmd]) % 256)
+        print('Starting keep alive thread')
+        keepAliveThread = threading.Thread(target=keepAliveWorker, args=(articulate_board,))
+        keepAliveThread.start()
 
-    ok_response = 'OKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOK'
-    ok_checksum = chr(sum([ord(x) for x in ok_response]) % 256)
+        print('Starting LRA command thread')
+        lraCmdThread = threading.Thread(target=lraCmdWorker, args=(articulate_board,))
+        lraCmdThread.start()
 
-    NUM_TRIALS = 20
-    HEADER_VAL = chr(253)
-    PACKET_SIZE = 40
-    OVERHEAD = 2
-    DATA_SIZE = PACKET_SIZE - OVERHEAD
-    MAX_NUM_ERRORS = 4
+        while(True):
+            print("Starting IMU server thread")
+            imuServerThread = threading.Thread(target=imuServerWorker, args=())
+            imuServerThread.start()
+            imuServerThread.join()
 
-    trial = 0
-    ok_success = False
-
-    while (trial < NUM_TRIALS and ok_success == False):
-        print('Trial', trial)
-
-        # send bytes
-        articulate_board.write(HEADER_VAL)
-        articulate_board.write(stream_cmd)
-        articulate_board.write(cmd_checksum)
-
-        # wait for reception
-        msg = ''
-        num_errors = 0
-        while((len(msg) < PACKET_SIZE) and (num_errors < MAX_NUM_ERRORS)):
-            received = articulate_board.read(timeout_sec=1)
-            if received is not None:
-                msg = msg + (received)
-            else:
-                num_errors=num_errors+1
-
-        expectedResponse = (HEADER_VAL + ok_response + ok_checksum)
-        ok_success = len(msg) == PACKET_SIZE and msg == expectedResponse
-
-    if (ok_success):
-        print('Successfully entered data stream mode')
-    else:
+    finally:
         target_device.disconnect()
-        print('Error: could not enter stream mode')
-        sys.exit()
 
-    read_error = False
-    while (not(read_error)):
-
-        # wait for reception
-        msg = ''
-        num_errors = 0
-        while((len(msg) < PACKET_SIZE) and (num_errors < MAX_NUM_ERRORS)):
-            received = articulate_board.read(timeout_sec=1)
-            if received is not None:
-                msg = msg + (received)
-            else:
-                num_errors=num_errors+1
-
-        print('Received:', msg)
-
-        read_error = num_errors == MAX_NUM_ERRORS
-
-    if (read_error):
-        print('Error: failed to read a package')
-
-    # except Exception, e:
-    #     print('Failed with Exception: \'{}\''.format(e))
-
-    # finally:
-        # Make sure device is disconnected on exit.
-    target_device.disconnect()
 
 # Initialize the BLE system.  MUST be called before other BLE calls!
 ble.initialize()
